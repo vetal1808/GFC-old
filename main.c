73450334f66a092e868bd7edd4672845e9e10cf3
vetal1808 <vetal1808@gmail.com>
2016-09-20 10:52:47 +0300

    next correct step to use i2c

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 #include "I2CRoutines.h"
+#include "BMP085_timer.h"
 int main(void)
 {
 	I2C_LowLevel_Init(I2C1);
-	uint8_t b = 1;
-	Status _stat = Error;
-	uint8_t a, i = 0;
-	for(a = 0; a < 127; a++)
-	{
-		_stat = I2C_Master_BufferWrite(I2C1, &b, 1, Polling, a<<1);
-		if(_stat)
-		{
-			i++;
-		}
-	}
 
+	BMP085_begin(BMP085_ULTRAHIGHRES);
+	/*
+	uint8_t b = 0xAA;
+	uint8_t rx_buff[16];
+	Status _stat = Error, _stat2 = Error;
+	_stat = I2C_Master_BufferWrite(I2C1, &b, 1, Polling, (0x77<<1));
+	_stat2 = I2C_Master_BufferRead(I2C1, rx_buff, 16, Polling, (0x77<<1));
+	 */
 
     while(1)
     {
