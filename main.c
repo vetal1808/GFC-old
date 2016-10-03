3f99874bca3dd7ff763c697c511293859fa29e94
vetal1808 <vetal1808@gmail.com>
2016-10-04 00:14:49 +0300

    nop

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 #include "stm32f10x.h"
 #include "stdint.h"
 #include "easy_uart.h"
@@ -9,15 +7,48 @@
 #include "BMP085_timer.h"
 #include "gps_parser.h"
 
+#define period 4000
+#define gyro1000_to_radians 1920.0
+
 
 int main()
 {
-	ESC_init();
-	uint16_t vals[] = {10,10,10,10};
+	init_timer();
+	USART_init(USART1);
+	I2C_LowLevel_Init(I2C1);
+	MPU6050_initialize();
+	MPU6050_setDLPFMode(0x03);
+	MPU6050_setFullScaleGyroRange(MPU6050_GYRO_FS_1000);
+	MPU6050_setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+	MPU6050_setSampleRateDiv(3);
+
+	int16_t ax = 0, ay = 0, az = 0;
+	int16_t gx = 0, gy = 0, gz = 0;
+	int16_t pitch, roll, yaw;
+	uint8_t buff[6];
+	uint32_t sync_time = micros();
+
 	while(1)
 	{
-		ESC_set_power(vals);
+		while(micros()-sync_time<period);
+		sync_time = micros();
+		MPU6050_getMotion6(&ax, &ay, &az, &gx, &gy, &gz, 0);
+		MadgwickAHRSupdateIMU((float)gx/gyro1000_to_radians, (float)gy/gyro1000_to_radians, (float)(gz)/(gyro1000_to_radians), ax, ay, az);
+		get_euclid(&pitch, &roll, &yaw);
+		send(pitch, 'A');
+		send(roll, 'B');
+		send(yaw, 'C');
+		send((micros()-sync_time), 'M');
+		USART_send(USART1,"W\n",2);
+
+
 	}
-
-
+}
+void send(int32_t val, char symb)
+{
+	uint8_t buf[6] = {0,0,0,0,0,0};
+	itoa(val, buf,10);
+	USART_send(USART1,&symb,1);
+	USART_send(USART1,buf,6);
+	USART_send(USART1,"\n",1);
 }
--- timer/timer.c
+++ timer/timer.c
@@ -2,24 +2,24 @@
 
 volatile uint16_t timer_overflow = 0;
 
-void TIM2_IRQHandler(void) //Функция обработчика прерывания от таймера 6
+void TIM4_IRQHandler(void) //Функция обработчика прерывания от таймера 6
 {
-    TIM2->SR &= ~TIM_SR_UIF; //Сбрасываем бит вызова прерывания.
+    TIM4->SR &= ~TIM_SR_UIF; //Сбрасываем бит вызова прерывания.
     timer_overflow++;
 }
 uint32_t micros()
 {
-	return (timer_overflow<<16) | (TIM2->CNT);
+	return (timer_overflow<<16) | (TIM4->CNT);
 }
 void init_timer()
 {
-	  NVIC_SetPriority(TIM2_IRQn, 1); //Приоритет прерывания
-	  NVIC_EnableIRQ(TIM2_IRQn); //Разрешаем обработку прерывания от таймера 2
-	  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;//Тактирование таймера TIM2
-	  TIM2->PSC = 72;
-	  TIM2->ARR = 0xFFFF;
-	  TIM2->DIER |= TIM_DIER_UIE; //Разрешаем прерывание при переполнении счетчика
-	  TIM2->CR1 |= TIM_CR1_CEN;//Запускаем счет
+	  NVIC_SetPriority(TIM4_IRQn, 1); //Приоритет прерывания
+	  NVIC_EnableIRQ(TIM4_IRQn); //Разрешаем обработку прерывания от таймера 2
+	  RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;//Тактирование таймера TIM4
+	  TIM4->PSC = 72;
+	  TIM4->ARR = 0xFFFF;
+	  TIM4->DIER |= TIM_DIER_UIE; //Разрешаем прерывание при переполнении счетчика
+	  TIM4->CR1 |= TIM_CR1_CEN;//Запускаем счет
 }
 void delay_us(uint32_t us)
 {
