877dd882237343a0ed810e38935835a0a9c67b00
vetal1808 <vetal1808@gmail.com>
2016-10-05 22:48:22 +0300

    тест интегрирования ускорения

--- MadgwickAHRS/MadgwickAHRS.h
+++ MadgwickAHRS/MadgwickAHRS.h
@@ -27,6 +27,7 @@ extern volatile float q0, q1, q2, q3;	// quaternion of sensor frame relative to 
 void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
 void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
 void get_euclid(int16_t * pitch, int16_t * roll, int16_t * yaw);
+void rotate_by_quatern(float * x, float * y, float * z);
 #endif
 //=====================================================================================================
 // End of file
--- main.c
+++ main.c
@@ -9,43 +9,13 @@
 
 #define period 4000
 #define gyro1000_to_radians 1920.0
-
+#define int2G_to_float 0.059814453125 // convert 16bit int data 2G sens to sm/sec^2 in float
 
 int main()
 {
 
 
 
-
-	USART_init(USART1);
-	init_timer();
-	I2C_LowLevel_Init(I2C1);
-	BMP085_begin(BMP085_ULTRAHIGHRES);
-
-	uint32_t start = micros();
-	int32_t alt = 0;
-	BMP085_set_zero_pressure(BMP085_meagure_press());
-	uint8_t skip = 0;
-	while(1)
-	{
-		BMP085_update();
-		alt = BMP085_get_altitude();
-
-		if(skip == 6){
-			send(alt, 'A');
-			skip = 0;
-		}
-		else {
-			skip++;
-		}
-
-		while(micros()-start < period);
-		start += period;
-	}
-
-
-
-	/*
 	init_timer();
 	USART_init(USART1);
 	I2C_LowLevel_Init(I2C1);
@@ -61,22 +31,39 @@ int main()
 	uint8_t buff[6];
 	uint32_t sync_time = micros();
 
+	float ax_bias=0, ay_bias=0, az_bias=15985;
+	float vx = 0, vy = 0, vz = 0;
 	while(1)
 	{
 		while(micros()-sync_time<period);
 		sync_time = micros();
 		MPU6050_getMotion6(&ax, &ay, &az, &gx, &gy, &gz, 0);
 		MadgwickAHRSupdateIMU((float)gx/gyro1000_to_radians, (float)gy/gyro1000_to_radians, (float)(gz)/(gyro1000_to_radians), ax, ay, az);
-		get_euclid(&pitch, &roll, &yaw);
-		send(pitch, 'A');
-		send(roll, 'B');
-		send(yaw, 'C');
+		float _ax = (float)ax, _ay = (float)ay, _az = (float)az;
+		rotate_by_quatern(&_ax, &_ay, &_az);
+
+		ax_bias += (_ax - ax_bias)*0.001;
+		ay_bias += (_ay - ay_bias)*0.001;
+		az_bias += (_az - az_bias)*0.001;
+
+		vx+=(_ax - ax_bias)*int2G_to_float*0.004;
+		vy+=(_ay - ay_bias)*int2G_to_float*0.004;
+		vz+=(_az - az_bias)*int2G_to_float*0.004;
+
+		send((int32_t)(vx*400.), 'A');
+		send((int32_t)(vy*400.), 'B');
+		send((int32_t)(vz*400.), 'C');
+
+		send((int32_t)((_ax - ax_bias)*10.), 'D');
+		send((int32_t)((_ay - ay_bias)*10.), 'E');
+		send((int32_t)((_az - az_bias)*10.), 'F');
+
 		send((micros()-sync_time), 'M');
 		USART_send(USART1,"W\n",2);
 
 
 	}
-	*/
+
 }
 void send(int32_t val, char symb)
 {
